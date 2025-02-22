e9f15742019a4129ccc13e105210982215b02b52
swathi-2114 <swthravichandran@gmail.com>
2025-02-22 10:17:09 +0800

    fixed adli shitty code, what a dumbass

--- Test.py
+++ Test.py
@@ -3,32 +3,32 @@ import json
 
 def printInfo(airport_data):
 
-    print(f"---{airport_data["airport_code"]} Info------------------------------------------------------------------------------------")
-    print(f"Airport Code: {airport_data["airport_code"]}\nAirport Name: {airport_data["airport_name"]}\nCity Name: {airport_data["city_name"]}" +
-          f"\nElevation: {airport_data["elevation"]}\nLatitude: {airport_data["latitude"]}\nLongtitude: {airport_data["longtitude"]}" +
-          f"\nTimezone: {airport_data["timezone"]}\n")
+    print(f"---{airport_data['airport_code']} Info------------------------------------------------------------------------------------")
+    print(f"Airport Code: {airport_data['airport_code']}\nAirport Name: {airport_data['airport_name']}\nCity Name: {airport_data['city_name']}" +
+          f"\nElevation: {airport_data['elevation']}\nLatitude: {airport_data['latitude']}\nLongtitude: {airport_data['longtitude']}" +
+          f"\nTimezone: {airport_data['timezone']}\n")
     printRoutesInfo(airport_data)
     print(f"---END-----------------------------------------------------------------------------------------\n")
     pass
 
 def printRoutesInfo(airport_data):
-    print("Routes: ")
-    for route in airport_data["routes"]:
-        print(f"Destination Airport Code: {route["airport_code"]}\nDistance (Km): {route["distance"]}\nDuration (Min): {route["duration"]}")
+    print('Routes: ')
+    for route in airport_data['routes']:
+        print(f"Destination Airport Code: {route['airport_code']}\nDistance (Km): {route['distance']}\nDuration (Min): {route['duration']}")
         getWeatherCondition(route)
         airlineStr = ""
-        for airline in route["airlines"]:
-            airlineStr += f"{airline["airline_name"]}({airline["airline_code"]}), "
+        for airline in route['airlines']:
+            airlineStr += f"{airline['airline_name']}({airline['airline_code']}), "
         airlineStr = airlineStr[:-2]
 
         print(f"Available Airlines: {airlineStr}\n")
     pass
 
 def getWeatherCondition(route_data):
-    print(f"Weather Condition: {route_data["weather_condition"]}")
+    print(f"Weather Condition: {route_data['weather_condition']}")
     routeDelayMultiplier = 1
 
-    match route_data["weather_condition"]:
+    match route_data['weather_condition']:
         case "Fine":
             print("Expect no delay to the flight time")
             routeDelayMultiplier = 1
@@ -50,8 +50,8 @@ try:
     file_data = json.load(file)
 
     print(f"Reading from {file_path}\n")
-    printInfo(file_data["TCA"]) # Print info for Airport code TCA
-    printInfo(file_data["SIN"]) # Print info for Airport code SIN
+    printInfo(file_data['TCA']) # Print info for Airport code TCA
+    printInfo(file_data['SIN']) # Print info for Airport code SIN
     file.close()
 except:
     print(f"Error: Unable to read {file_path}")
