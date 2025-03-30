2cc841134b5cbddd1bcfc6b33bd6f6e89c83cca9
subhaa2 <101237120+subhaa2@users.noreply.github.com>
2025-03-30 12:18:59 +0800

    changed date error again

--- MapRoutes.py
+++ MapRoutes.py
@@ -325,26 +325,20 @@ class FirstScreen(Screen):
             self.ids.date_error.text = "[color=ff3333]Flight date is required[/color]"
             has_errors = True
         
-        # Get today's date
-        today = date.today()
-
-        # Add 1 year (365 days)
-        one_year_later = today + timedelta(days=365)
-
-        # Format the result
-        formatted_date = one_year_later.strftime("%d/%m/%Y")
-
-        # Validate flight date
-        if not flight_date:
-            self.ids.date_error.text = "[color=ff3333]Flight date is required[/color]"
+        try:
+            flight_date_obj = datetime.strptime(flight_date, "%d/%m/%Y").date()
+            today = date.today()
+            one_year_later = today + timedelta(days=365)
+            if flight_date_obj < today:
+                self.ids.date_error.text = "[color=ff3333]Flight date cannot be earlier than today[/color]"
+                has_errors = True
+            elif flight_date_obj > one_year_later:
+                self.ids.date_error.text = "[color=ff3333]Flight date must be within 1 Year[/color]"
+                has_errors = True
+        except ValueError:
+            self.ids.date_error.text = "[color=ff3333]Invalid date format[/color]"
             has_errors = True
-        elif flight_date < date.today().strftime("%d/%m/%Y"):
-            self.ids.date_error.text = "[color=ff3333]Flight date cannot be earlier than today[/color]"
-            has_errors = True
-        elif flight_date > formatted_date:
-            self.ids.date_error.text = "[color=ff3333]Flight date must be within 1 Year[/color]"
-            has_errors = True
-        
+
         if has_errors:
             return
 
